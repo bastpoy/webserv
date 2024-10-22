c8803d747ebac7b31b2f8d8c2acb71444c97dc78
bastpoy <bastien.poyet@outlook.fr>
2024-10-22 19:12:25 +0200

    test du serveur avec des ficheirs statiques

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <errno.h> // For errno
 #include <string.h> // For strerror
 #include <sstream> // For string streams
+#include <fstream>      // std::ifstream
+#include <stdexcept>
+#include <map>
 
 template <typename T>
 std::string to_string(T value) {
@@ -14,71 +17,135 @@ std::string to_string(T value) {
   return oss.str();
 }
 
+std::string readFile(std::string &path)
+{
+    std::string content;
+    std::string finalContent;
+    std::ifstream file(path.c_str());
+    if(!file.is_open())
+    {
+        throw std::runtime_error("error opening file:" + path);
+    }
+    while(std::getline(file, content))
+    {
+        finalContent = finalContent + content;
+    }
+    return (finalContent);
+}
+
+void getFileContent(std::string& content)
+{
+    std::string defaultPath = "./website";
+    std::string filePath = defaultPath + "/index.html"; // Change this to your file path
+    
+    try 
+    {
+        content = readFile(filePath);
+    } 
+    catch (const std::exception& e) 
+    {
+        std::cerr << "Error reading file: " << e.what() << std::endl;
+    }    
+}
+
+std::string getContentType(const std::string &path) 
+{
+    std::map<std::string, std::string> contentTypes;
+
+    contentTypes.insert(std::pair<std::string, std::string>(".html", "text/html"));
+    contentTypes.insert(std::pair<std::string, std::string>(".css", "text/css"));
+    contentTypes.insert(std::pair<std::string, std::string>(".js", "application/javascript"));
+    contentTypes.insert(std::pair<std::string, std::string>(".png", "image/png"));
+    contentTypes.insert(std::pair<std::string, std::string>(".jpg", "image/jpeg"));
+    contentTypes.insert(std::pair<std::string, std::string>(".gif", "image/gif"));
+
+    size_t dotPos = path.find_last_of(".");
+    if (dotPos != std::string::npos) {
+        std::string extension = path.substr(dotPos);
+        if (contentTypes.find(extension) != contentTypes.end()) {
+        return contentTypes[extension];
+        }
+    }
+    return "application/octet-stream"; // Default content type
+}
+
 int main() {
   // Create a socket (IPv4, TCP)
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
     std::cerr << "Failed to create socket. errno: " << errno << std::endl;
     exit(EXIT_FAILURE);
-  }
+    }
 
-  // Bind the socket to an address and port
-  sockaddr_in sockaddr;
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = INADDR_ANY;
-  sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to network byte order
-  if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
-    std::cerr << "Failed to bind to port 9999. errno: " << strerror(errno) << std::endl;
-    exit(EXIT_FAILURE);
-  }
+    // Bind the socket to an address and port
+    sockaddr_in sockaddr;
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+    sockaddr.sin_port = htons(9999); // htons is necessary to convert a number to network byte order
+    if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) 
+    {
+        std::cerr << "Failed to bind to port 9999. errno: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
-  // Start listening. Hold at most 10 connections in the queue
-    if (listen(sockfd, 10) < 0) {
-      std::cerr << "Failed to listen on socket. errno: " << strerror(errno) << std::endl;
-      exit(EXIT_FAILURE);
+    // Start listening. Hold at most 10 connections in the queue
+    if (listen(sockfd, 10) < 0) 
+    {
+        std::cerr << "Failed to listen on socket. errno: " << strerror(errno) << std::endl;
+        exit(EXIT_FAILURE);
     }
     std::cout << "Server is listening on port 9999..." << std::endl;
 
-  while(true)
-  {
-    // Accept a connection
-    socklen_t addrlen = sizeof(sockaddr);  
-    int connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
-    if (connection < 0) {
-      std::cerr << "Failed to grab connection. errno: " << errno << std::endl;
-      exit(EXIT_FAILURE);
+    while(true)
+    {
+        // Accept a connection
+        socklen_t addrlen = sizeof(sockaddr);  
+        int connection = accept(sockfd, (struct sockaddr*)&sockaddr, &addrlen);
+        if (connection < 0) {
+            std::cerr << "Failed to grab connection. errno: " << errno << std::endl;
+            exit(EXIT_FAILURE);
+        }
+        std::cout << "Connection accepted." << std::endl;
+
+        // Receive data from the connection
+        char buffer[100];
+        ssize_t bytesRead = recv(connection, buffer, sizeof(buffer) - 1, 0);
+        if (bytesRead < 0) 
+        {
+            std::cerr << "Error reading data. errno: " << strerror(errno) << std::endl;
+            return (errno);
+        } 
+        else {
+            buffer[bytesRead] = '\0'; // Null-terminate the buffer
+            // std::cout << "Received message: " << buffer << std::endl;
+        }
+        std::string path = buffer;
+        std::string content;
+        std::string contentType;
+
+        // je recupere le path de chaque requete http
+        std::cout << "the path before: " << path << std::endl;
+        path = path.substr(path.find('/'), path.size() - path.find('/'));
+        path = path.substr(0, path.find(' '));
+        // std::cout << "the path is: " << path << std::endl;
+
+        // je recuper le contenu du fichier
+        getFileContent(content);
+        // je recupere le type du fichier 
+        contentType = getContentType(path);
+        std::cout << "the content type: " << contentType << std::endl;
+        std::string response = "HTTP/1.1 200 OK \r\n"
+                                "Content-Type: text/html\r\n"
+                                "Content-Type: " + contentType + ";\r\n"
+                                // "Content-Length: " + to_string(content.size()) + "\r\n"
+                                "Connection: close\r\n"
+                                "\r\n" + content;
+        std::cout << "response\n";
+        send(connection, response.c_str(), response.size(), 0);
+        close(connection);
+        std::cout << "\n\n";
     }
-    std::cout << "Connection accepted." << std::endl;
+    close(sockfd);
 
-    // Receive data from the connection
-    char buffer[100];
-    ssize_t bytesRead = recv(connection, buffer, sizeof(buffer) - 1, 0);
-    if (bytesRead < 0) {
-      std::cerr << "Error reading data. errno: " << strerror(errno) << std::endl;
-    } else {
-      buffer[bytesRead] = '\0'; // Null-terminate the buffer
-      std::cout << "Received message: " << buffer << std::endl;
-    }
-
-    // Send a response to the sockfd
-    // std::string response = "Hello from server\n";
-    // std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-    //                             "<html><body><h1>Hello, World!</h1></body></html>";
-
-    std::string response_body = "<html><body><h1>Hello, World!</h1></body></html>";
-    std::string response = "HTTP/1.1 200 OK\r\n"
-                           "Content-Type: text/html\r\n"
-                           "Content-Length: " + to_string(response_body.size()) + "\r\n"
-                           "Connection: close\r\n"
-                           "\r\n" + response_body;
-    std::cout << "\nResponse:\n" << response;
-    send(connection, response.c_str(), response.size(), 0);
-    close(connection);
-    std::cout << "\n\n";
-  }
-
-  // Close the connections
-  close(sockfd);
-
-  return 0;
+    return 0;
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # WEBSERV
 
-## Command understanding
+## Comprehension des commandes
 
 - epoll : permet de gerer chaque fd concernant chaque requete
     
@@ -15,3 +15,80 @@
         uint32_t  u32;
         uint64_t  u64;
     };
+
+
+## NOTION HTTP
+
+- Comprendre la RFC et implementer les differentes fonctionnalites de cette norme
+
+
+### FORMAT DE BASE
+
+Ligne de début (start-line)
+En-têtes (headers)
+Ligne vide (CRLF)
+Corps du message (body) optionnel
+
+### REQUETE
+
+GET / HTTTP/1.1
+
+premier argument: methode (post ou get)
+deuxieme argument: chemin apres mon nom de domaine
+troisieme argument: version HTTP
+
+**En-Tetes requete**
+
+Host (obligatoire)
+User-Agent
+Accept
+Accept-Language
+Accept-Encoding
+Authorization
+
+### REPONSE
+
+
+**En-Tetes reponses**
+
+Server
+Content-Type
+Content-Length
+Content-Encoding
+Location
+
+### CODE ERREUR ET SUCCES
+
+1xx : Information
+2xx : Succès
+3xx : Redirection
+4xx : Erreur Client
+5xx : Erreur Serveur
+
+## FONCTIONNALITES A IMPLEMENTER
+
+- Repertoire de fichiers statiques
+- Redirection HTTP
+- Accepter des fichiers
+
+## CGI
+
+### FONCTIONNALITES
+
+- Implementer un CGI suivant l'extension du fichier demande.
+- Le faire fonctionner avec GET et POST.
+
+### EXPLICATIONS
+
+- C'est une option sur un serveur qui va permettre de gerer des requetes specifiques comme des passages de script en parametre
+
+## TACHES
+
+- Comprendre la RFC sur http et les en-tetes requete et reponses
+- Comment implementer epoll pour le multiplexing
+- Commencer par gerer les simples requetes
+- Decouper mes requetes en examinant le header de la requete
+- Pouvoir GET POST DELETE
+- Comprendre le CGI et comment l'implemeneter
+- Setup routes http?
+- Activer, desactiver le listing des repertoires?
