76af1037aed5c5402613060655acaa2b6fee5de3
avg38 <mailamandineg@gmail.com>
2024-12-12 16:12:20 +0100

    fix website

--- www/pages/cgi-bin/script.js
+++ www/pages/cgi-bin/script.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 
-console.log("<html><head><title>Test CGI avec JavaScript</title><link rel=\"stylesheet\" href=\"../../styles.css\"></head><body style=\"text-align:center\"><nav style=\"padding:15px;\"><a href=\"../../index.html\" style=\"font-size: 20px;margin:1rem;width:150px;padding:5px;\">Home</a><a href=\"../../cgi.html\" style=\"font-size: 20px;margin:1rem;width:150px;padding:5px;\">CGI</a></nav><h1>JavaScript CGI executed with success !</h1><p>Server has correctly handle .js file</p><a href=\"../../index.html\" id=\"return\">Go Back Home</a></body></html>");
+console.log("<html><head><title>Test CGI avec JavaScript</title><link rel=\"stylesheet\" href=\"../../styles.css\"></head><body><h1>JavaScript CGI executed with success !</h1><p>Server has correctly handle .js file</p><a href=\"../../index.html\" id=\"return\">Go Back Home</a></body></html>");
--- www/pages/cgi-bin/script.php
+++ www/pages/cgi-bin/script.php
@@ -6,11 +6,7 @@ header("Content-Type: text/html");
 // Afficher les informations de l'environnement CGI
 echo "<html>";
 echo "<head><title>CGI Test</title><link rel=\"stylesheet\" href=\"../../styles.css\"></head>";
-echo "<body style=\"text-align:center\">";
-echo "<nav style=\"padding:15px;\">";
-echo "<a href=\"../../index.html\" style=\"font-size: 20px;margin:1rem;width:150px;padding:5px;\">Home</a>";
-echo "<a href=\"../../cgi.html\" style=\"font-size: 20px;margin:1rem;width:150px;padding:5px;\">CGI</a>";
-echo "</nav>";
+echo "<body>";
 echo "<h1>PHP CGI executed with success !</h1>";
 echo "<p>Server has correctly handle .php file</p>";
 echo "<a href=\"../../index.html\" id=\"return\">Go Back Home</a>";
